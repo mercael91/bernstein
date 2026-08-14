ddb2774c2a4e40672bfbb62decd7570652e3c67c
mercael <mercael91@gmail.com>
2026-08-14 18:59:21 +0300

    Fix over-reporting capacity by preventing re-registration of restarting workers

--- src/bernstein/core/routes/task_cluster.py
+++ src/bernstein/core/routes/task_cluster.py
@@ -1,7 +1,10 @@
 def register_node(request):
     node_id = request.json.get('node_id')
-    if node_id and NodeInfo.exists(node_id):
-        node_info = NodeInfo.get(node_id)
-    else:
+    if node_id:
+        existing_node = NodeInfo.query.filter_by(node_id=node_id).first()
+        if existing_node:
+            return jsonify({'message': 'Node already registered'}), 200
         node_info = NodeInfo(node_id=node_id)
+    else:
+        node_info = NodeInfo()
     # ... rest of the function ...
