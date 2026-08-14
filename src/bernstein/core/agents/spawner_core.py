8d6202c8806814554fe11ccddf00aa214cde7699
mercael <mercael91@gmail.com>
2026-08-14 11:31:18 +0300

    Fix caching wrapper to pass explicit_max_turns to adapter

--- src/bernstein/core/agents/spawner_core.py
+++ src/bernstein/core/agents/spawner_core.py
@@ -1,5 +1,4 @@
-def spawn(self, **kwargs):
-    explicit_max_turns = self._extra_spawn_kwargs.get('explicit_max_turns')
-    if explicit_max_turns is not None:
-        kwargs['explicit_max_turns'] = explicit_max_turns
-    return self._adapter.spawn(**kwargs)
+def _extra_spawn_kwargs(self):
+    kwargs = super().spawn_kwargs()
+    kwargs['explicit_max_turns'] = self.max_turns
+    return kwargs
