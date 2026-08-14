93613e111759930bf860d91dbb810fd6ca31d593
mercael <mercael91@gmail.com>
2026-08-14 13:08:31 +0300

    Fix explicit_max_turns being dropped by CachingAdapter

--- src/bernstein/core/agents/spawner_core.py
+++ src/bernstein/core/agents/spawner_core.py
@@ -1,4 +1,5 @@
-def _extra_spawn_kwargs(self):
-    kwargs = super().spawn_kwargs()
-    kwargs['explicit_max_turns'] = self.max_turns
+def _extra_spawn_kwargs(self, task: Task) -> Dict[str, Any]:
+    kwargs = {}
+    if "explicit_max_turns" in inspect.signature(self.adapter.spawn).parameters:
+        kwargs["explicit_max_turns"] = task.max_turns
     return kwargs
